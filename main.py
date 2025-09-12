345c32ac514cfbc273e5168e83b4c3b990390d1c
jjvanderuitenbeek <jjvanderuitenbeek@gmail.com>
2025-09-12 15:11:59 +0200

    Receiptreaders AH and Lidl 0.9 EN streamlit app

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 
+import logging
+import subprocess
+
 import src.receipt_intelligence.infrastructure.init_db as init_db
+from src.receipt_intelligence.modules.receipt_reader.receiptReader import AHReceiptProcessor, LidlReceiptProcessor
 
-from src.receipt_intelligence.config import settings
-from src.receipt_intelligence.application.receipt_reader.receiptReader import AHReceiptProcessor, LidlReceiptProcessor
-
+from src.receipt_intelligence.interface.streamlit import app
 
 # .venv\Scripts\activate.bat
-# python src/main.py
+# python main.py
 
 
 def main():
 
+
+    logging.basicConfig(level=logging.INFO)
     init_db.init_db()
 
-    r = AHReceiptProcessor()
-    r.process_all_receipts()    
+    rpAH = AHReceiptProcessor()
+    rpAH.process_all_receipts()    
 
-    #r = LidlReceiptProcessor() 
-      
-    
+    rpLidl = LidlReceiptProcessor() 
+    rpLidl.process_all_receipts() 
+   
+    # Start Streamlit
+    subprocess.run(["streamlit", "run", "src/receipt_intelligence/interface/streamlit/app.py"])
+
 
 if __name__ == "__main__":
     main()
