796cc714aad21dd87a64cb19df8336cb65eb50a7
jjvanderuitenbeek <jjvanderuitenbeek@gmail.com>
2025-09-20 16:31:59 +0200

    added execute query

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ from src.receipt_intelligence.modules.receipt_reader.receiptReader import AHRece
 
 from src.receipt_intelligence.interface.streamlit import app
 
+"""
+import os
+from pathlib import Path
+import sys
+
+project_root = Path().resolve() 
+print(project_root)
+sys.path.append(str(project_root))
+"""
+
 # .venv\Scripts\activate.bat
-# python main.py
+# python main.py or python chatbot.py
 # docker build -t my-python-app:latest .
 # docker run -p 8501:8501 my-python-app
 
